a02e1f27f651c99b263211adc21f2c47775be87e
Raihan Taher <rayhan.taher@gmail.com>
2020-08-07 04:57:14 +0600

    im done for tonight

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require("mongoose");
+
 const app = express();
+
+// DB config
+
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://TheRaihan:<password>@cluster0.iojlf.mongodb.net/<dbname>?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true});
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
