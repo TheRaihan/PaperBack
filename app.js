0caa04f4c35e08ea2ce3f0d727e2d3d417e3bab0
Raihan Taher <rayhan.taher@gmail.com>
2020-08-23 03:42:45 +0600

    created a mess...
    no worries.. it will work after passing couple of sleepless nights :3

--- app.js
+++ app.js
@@ -1,59 +1,61 @@
-const express = require('express');
-const expressLayouts = require('express-ejs-layouts');
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
 const mongoose = require("mongoose");
-const passport = require('passport');
-const flash = require('connect-flash');
-const session = require('express-session');
-
+const passport = require("passport");
+const flash = require("connect-flash");
+const session = require("express-session");
 
 const app = express();
 
+const mongoConnect = require("./util/database").mongoConnect;
+
 // Passport Config
-require('./config/passport')(passport);
+require("./config/passport")(passport);
 
 // DB config
 // const db = require('./config/keys').MongoURI;
-const db = 'mongodb+srv://TheRaihan:paperback@cluster0.iojlf.mongodb.net/<dbname>?retryWrites=true&w=majority';
+const db =
+  "mongodb+srv://TheRaihan:paperback@cluster0.iojlf.mongodb.net/PaperBack?retryWrites=true&w=majority";
 
-mongoose.connect(db, { useNewUrlParser:true, useUnifiedTopology: true })
-.then(() => console.log("MongoDB connected"))
-.catch((err) => console.log(err));
-
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
 
 app.use(expressLayouts);
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 // Body Parser
-app.use(express.urlencoded({extended:false }));
+app.use(express.urlencoded({ extended: false }));
 
-// app.use 
+// app.use
 // Express session
 app.use(
-    session({
-      secret: 'secret',
-      resave: true,
-      saveUninitialized: true
-    })
-  );
-  
-  // Passport middleware
-  app.use(passport.initialize());
-  app.use(passport.session());
-  
-  // Connect flash
-  app.use(flash());
-  
-  // Global variables
-  app.use(function(req, res, next) {
-    res.locals.success_msg = req.flash('success_msg');
-    res.locals.error_msg = req.flash('error_msg');
-    res.locals.error = req.flash('error');
-    next();
-  });
-  
-app.use('/', require('./routes/index'));
+  session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 
-app.use('/users', require('./routes/users'));
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Connect flash
+app.use(flash());
+
+// Global variables
+app.use(function (req, res, next) {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  res.locals.error = req.flash("error");
+  next();
+});
+
+app.use("/", require("./routes/index"));
+
+app.use("/users", require("./routes/users"));
 //app.use('/',require('./routes/dass'));
 
-app.listen(5000);
+mongoConnect(() => app.listen(5000));
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,45 @@
-const express = require('express');
-const passport = require('passport');
+const express = require("express");
+const passport = require("passport");
 const router = express();
 
+const Product = require("../models/product");
 
-router.get('/', (req,res,next) => res.render('welcome'));
-router.get('/dashboard', (req,res,next) => res.render('dashboard'));
+router.get("/", (req, res, next) => res.render("welcome"));
+
+router.get("/index", (req, res, next) => {
+  Product.fetchAll()
+    .then((products) => {
+      res.render("index", {
+        prods: products,
+        pageTitle: "Home",
+        path: "/index",
+      });
+    })
+    .catch((err) => console.log(err));
+});
+
+router.get("/addBook", (req, res, next) => {
+  res.render("addBook", {
+    pageTitle: "Add Product",
+    path: "/addBook",
+  });
+});
+
+router.post("/addBook", (req, res, next) => {
+  const title = req.body.title;
+  const imgURL = req.body.imgURL;
+  const price = req.body.price;
+  const des = req.body.des;
+  const product = new Product(title, price, des, imgURL);
+  product
+    .save()
+    .then((result) => {
+      console.log("Created Product");
+      res.redirect("addBook");
+    })
+    .catch((err) => console.log(err));
+
+  res.redirect("/");
+});
+
 module.exports = router;
- 
- 
