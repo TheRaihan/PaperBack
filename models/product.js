e967a8664d6359f984a6672bc5c2abe81ede8cfd
Raihan Taher <rayhan.taher@gmail.com>
2020-08-27 17:33:41 +0600

    MVC pattern

--- models/product.js
+++ models/product.js
@@ -1,3 +1,28 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  des: {
+    type: String,
+    required: false,
+  },
+  imgURL: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('Product',productSchema);
+
 const getDb = require("../util/database").getDb;
 
 class Product {
--- routes/users.js
+++ routes/users.js
@@ -1,97 +1,18 @@
 const express = require("express");
 const router = express.Router();
-const bcrypt = require("bcryptjs");
-const passport = require("passport");
-
-const User = require("../models/User");
+const loginCntrl = require("../controller/login");
 
 router.get("/login", (req, res, next) => res.render("login"));
 
 router.get("/register", (req, res, next) => res.render("register"));
 
 // Register handle
-router.post("/register", (req, res) => {
-  // console.log(req.body);
-  const { name, email, password, password2 } = req.body;
-
-  let errors = [];
-
-  if (!name || !email || !password || !password2)
-    errors.push({ msg: "Please fill all fields" });
-
-  if (password != password2) errors.push({ msg: "Password didn't match" });
-
-  if (email.endsWith("ewubd.edu") != true)
-    errors.push({ msg: "Enter your EWU mail" });
-
-  if (errors.length > 0) {
-    res.render("register", {
-      errors,
-      name,
-      email,
-      password,
-      password2,
-    });
-  } else {
-    User.findOne({ email: email }).then((user) => {
-      if (user) {
-        //user exists
-        errors.push({ msg: "Email is already registered" });
-        res.render("register", {
-          errors: errors,
-          name: name,
-          email: email,
-          password: password,
-          password2: password2,
-        });
-      } else {
-        const newUser = new User({
-          name: name,
-          email: email,
-          password: password,
-        });
-
-        // console.log(newUser)
-        // res.send('hello');
-
-        //hash password
-        bcrypt.genSalt(10, (err, salt) =>
-          bcrypt.hash(newUser.password, salt, (err, hash) => {
-            if (err) throw err;
-
-            newUser.password = hash;
-
-            newUser
-              .save()
-              .then((user) => {
-                req.flash(
-                  "success_msg",
-                  "You are now registered and Can Log in"
-                );
-                res.redirect("/users/login");
-              })
-              .catch((err) => console.log(err));
-          })
-        );
-      }
-    });
-  }
-});
+router.post("/register", loginCntrl.postRegister);
 
 //Login
-router.post("/login", (req, res, next) => {
-  passport.authenticate("local", {
-    successRedirect: "/index",
-    failureRedirect: "/users/login",
-    failureFlash: true,
-  })(req, res, next);
-});
-//logout
+router.post("/login", loginCntrl.postLogin);
 
-router.get("/logout", (req, res, next) => {
-  req.logout();
-  req.flash("success_msg", "You have successfully logged out");
-  res.redirect("/users/login");
-});
+//logout
+router.get("/logout", loginCntrl.logout);
 
 module.exports = router;
