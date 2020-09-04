cb2066e7d4b76905a3cf53f800692efca4e7c28b
Raihan Taher <rayhan.taher@gmail.com>
2020-09-05 02:00:39 +0600

    update css

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express();
 const { ensureAuthenticated } = require("../config/auth");
 const intrfc = require("../controller/interface");
 
-router.get("/", (req, res, next) => res.render("login"));
+router.get("/", (req, res, next) => res.render("hi"));
 
 router.get("/index", ensureAuthenticated, intrfc.home);
 
