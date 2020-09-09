74f8ed5ed589a64e1f1436c224ff31843ee978b4
Safia Jahan Priota <jahan.priota595@gmail.com>
2020-09-10 00:15:55 +0600

    errorpge

--- app.js
+++ app.js
@@ -58,7 +58,11 @@ app.use("/users", require("./routes/users"));
 //app.use('/',require('./routes/dass'));
 
 app.use((req, res, next) => {
-  res.status(404).send("<h1>Error page</h1>");
+  res.status(404).render("404", {
+    name: req.user.name,
+    userID: req.user._id,
+    ERROR: "Page Not Found",
+  });
 });
 
 app.listen(5000);
